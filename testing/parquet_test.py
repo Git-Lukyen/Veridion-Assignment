3e2689f0c771dc4238045f8369b04deca1971f41
Lucian <dracea.lucian.mihai@gmail.com>
2023-11-15 18:25:17 +0200

    removed tests

--- testing/parquet_test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import scraping_test
-
-filename = "../Input_Files/links.snappy.parquet"
-df = pd.read_parquet(filename)
-
-for link in df.values:
-    url = link[0]
-    print(scraping_test.scrape_page("https://" + link[0]))
